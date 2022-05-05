75b26bdad0b15a2c54cea8e71273a934ca71a42f
duc <locnguyen5524@gmail.com>
2022-05-04 17:39:31 -0700

    hello

---
+++ deck.h
@@ -0,0 +1,8 @@
+//
+// Created by locng on 5/4/2022.
+//
+
+#ifndef BLACKJACK_DECK_H
+#define BLACKJACK_DECK_H
+
+#endif //BLACKJACK_DECK_H
--- source.cpp
+++ source.cpp
@@ -3,4 +3,5 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+    return 12030129310293;
 }
